48d4781602a3c2b9aa176ddd6adf256e41b7ba3b
jawadmn <jawadmn@gmail.com>
2015-01-29 12:26:51 -0500

    Added elevation:0 to the toolbar so Lollipop phones don't add their own elevation

---
+++ app/src/main/java/com/mdstudios/themgoals/ActivityGoalAdder.java
@@ -0,0 +1,12 @@
+package com.mdstudios.themgoals;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by jawad on 28/01/15.
+ *
+ * Purpose: Handles adding "Goals" to the app
+ */
+public class ActivityGoalAdder extends ActionBarActivity {
+}
