3a331cfaa5dbb07bef44d58bc59b6992c0822c6a
jawadmn <jawadmn@gmail.com>
2015-02-04 23:07:08 -0500

    New file: Interacts with the Goals database

---
+++ app/src/main/java/com/mdstudios/themgoals/Goals/GoalsDbAdapter.java
@@ -0,0 +1,10 @@
+package com.mdstudios.themgoals.Goals;
+
+/**
+ * Created by jawad on 04/02/15.
+ *
+ * Interacts with the Goals database for all the beloved users
+ */
+public class GoalsDbAdapter {
+    
+}
