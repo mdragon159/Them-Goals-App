ad29716c9a62d0f625a00cfbafc120cdd6aafb3e
jawadmn <jawadmn@gmail.com>
2015-02-13 23:41:03 -0500

    Visual fixes: Removed drop shadow under toolbar (tab host now there and getting too complicated this early just to get the effect correct as the app changes) and fixed color differences of the tabs

--- app/src/main/java/com/mdstudios/themgoals/Core/HomeFragment.java
+++ app/src/main/java/com/mdstudios/themgoals/Core/HomeFragment.java
@@ -26,7 +26,7 @@ public class HomeFragment extends Fragment implements MaterialTabListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_slidingtabssample, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
