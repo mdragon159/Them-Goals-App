679bf28e56def846fb8446071115553010282ef2
jawadmn <jawadmn@gmail.com>
2015-02-15 14:06:15 -0500

    Added catch for well-known bug of first frame not animating correctly (yFraction corrected only)

--- app/src/main/java/com/mdstudios/themgoals/Utils/RelativeLayoutAnim.java
+++ app/src/main/java/com/mdstudios/themgoals/Utils/RelativeLayoutAnim.java
@@ -2,6 +2,7 @@ package com.mdstudios.themgoals.Utils;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
 
 /**
@@ -10,6 +11,9 @@ import android.widget.RelativeLayout;
  * Purpose: Support fragment translate transitions
  */
 public class RelativeLayoutAnim extends RelativeLayout {
+    private float yFraction = 0;
+    private ViewTreeObserver.OnPreDrawListener preDrawListener = null;
+
     public RelativeLayoutAnim(Context context) {
         super(context);
     }
@@ -32,7 +36,25 @@ public class RelativeLayoutAnim extends RelativeLayout {
         setX((width > 0) ? (xFraction * width) : -9999);
     }
 
-    public void setYFraction(final float fraction) {
+    public void setYFraction(float fraction) {
+
+        this.yFraction = fraction;
+
+        if (getHeight() == 0) {
+            if (preDrawListener == null) {
+                preDrawListener = new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        getViewTreeObserver().removeOnPreDrawListener(preDrawListener);
+                        setYFraction(yFraction);
+                        return true;
+                    }
+                };
+                getViewTreeObserver().addOnPreDrawListener(preDrawListener);
+            }
+            return;
+        }
+
         float translationY = getHeight() * fraction;
         setTranslationY(translationY);
     }
