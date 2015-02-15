a8ba5f46d40b52688a67848626d485b7d143df96
jawadmn <jawadmn@gmail.com>
2015-02-15 12:50:04 -0500

    Created/refactored GoalAdderFragment, button on GoalMainFragment actually does something, and changed toolbar shadows to its own little layout file to avoid copy and pasting the same few lines over and over again

---
+++ app/src/main/java/com/mdstudios/themgoals/Goals/GoalAdderFragment.java
@@ -0,0 +1,12 @@
+package com.mdstudios.themgoals.Goals;
+
+import android.app.Fragment;
+
+/**
+ * Created by jawad on 28/01/15.
+ *
+ * Purpose: Handles adding "Goals" to the app
+ */
+public class GoalAdderFragment extends Fragment {
+
+}
--- app/src/main/java/com/mdstudios/themgoals/Goals/GoalsMainFragment.java
+++ app/src/main/java/com/mdstudios/themgoals/Goals/GoalsMainFragment.java
@@ -6,16 +6,32 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.AddFloatingActionButton;
 import com.mdstudios.themgoals.R;
 
 /**
  * Created by jawad on 13/02/15.
  */
 public class GoalsMainFragment extends Fragment {
+    AddFloatingActionButton mAddButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_goals, container, false);
+        return inflater.inflate(R.layout.fragment_goals_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Cache all the necessary views
+        mAddButton = (AddFloatingActionButton) view.findViewById(R.id.add_button);
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Clicked zeh button!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
